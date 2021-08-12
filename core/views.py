0b19530e6fc3600d55b0f89a8c70c9f5dc8752cf
GeeB01 <gabrielsilveira3675@gmail.com>
2021-08-12 12:40:30 -0300

    Criado tela de listagem de eventos e redirecionamentos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from core.models import Evento
 # Create your views here.
+
+# funcao para redirecionar a aplicação para outra pagina
+#    def index(request):
+#        return redirect('/admin/')
+# fim da funao
+
+def lista_eventos(request):
+    # usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos':evento}
+    return render(request,'agenda.html', dados)
